5d093fe501b420ba873d92aef98b97ad67bb0781
lululthfh <punyalulu05@gmail.com>
2025-10-23 08:47:11 +0700

    feat: menambahkan layout Column pada layout Row dalam Card Component di dalam Column pada function ActivitasPertama

--- app/src/main/java/com/example/prak3_209/UIPertama.kt
+++ app/src/main/java/com/example/prak3_209/UIPertama.kt
@@ -48,6 +48,7 @@ fun ActivitasPertama(modifier: Modifier){
                     modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
                 )
                 Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {  }
             }
         }
     }
