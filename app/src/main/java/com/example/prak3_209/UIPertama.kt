3c2c9532fc9f4646979d3607cdb6360d15cc4a9d
lululthfh <punyalulu05@gmail.com>
2025-10-23 09:00:20 +0700

    feat: menambahkan komponen Text pada layout Box dalam layout Column pada function ActivitasPertama dengan mengambil resource string dengan id copy

--- app/src/main/java/com/example/prak3_209/UIPertama.kt
+++ app/src/main/java/com/example/prak3_209/UIPertama.kt
@@ -68,7 +68,12 @@ fun ActivitasPertama(modifier: Modifier){
             }
         }
         Box(modifier = Modifier
-            .fillMaxSize()){}
+            .fillMaxSize()){
+            Text(text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp))
+        }
     }
 
 }
