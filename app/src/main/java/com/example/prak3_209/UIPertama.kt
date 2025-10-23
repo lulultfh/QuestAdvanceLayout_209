98ce072f4581280bebc079a2b2cde0dced4c1c3f
lululthfh <punyalulu05@gmail.com>
2025-10-23 08:20:29 +0700

    feat: menambahkan layout Column dengan properties komponen modifier dengan jarak atas 100, fillMaxSize dan horizontalAlignment Center Horizontally

--- app/src/main/java/com/example/prak3_209/UIPertama.kt
+++ app/src/main/java/com/example/prak3_209/UIPertama.kt
@@ -1,9 +1,17 @@
 package com.example.prak3_209
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top = 100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {  }
 
 }
