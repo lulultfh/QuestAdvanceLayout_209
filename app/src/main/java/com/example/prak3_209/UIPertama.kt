398faf9705da1ae21501ba85a1ff16c9f6026cc7
lululthfh <punyalulu05@gmail.com>
2025-10-24 12:54:08 +0700

    feat: menambahkan komponen Text ketiga dengan mengambil resource string dengan id notelp2 di dalam layout Column pada layout Row dalam Card Component ketiga dalam layout COlumn pada function ActivitasPertama

--- app/src/main/java/com/example/prak3_209/UIPertama.kt
+++ app/src/main/java/com/example/prak3_209/UIPertama.kt
@@ -134,6 +134,12 @@ fun ActivitasPertama(modifier: Modifier){
                         color = Color.Cyan,
                         modifier = Modifier.padding(top = 8.dp)
                     )
+                    Text(
+                        text = stringResource(id = R.string.notelp2),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
                 }
             }
         }
